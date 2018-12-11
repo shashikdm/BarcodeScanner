08f6ee84f1402390be8573cde3bbe6f2f35b0b29
shashikdm <shashikant.123kadam@gmail.com>
2018-12-11 10:49:18 +0530

    minute change

--- app/src/main/java/com/android/shashi/barcodescanner/Result.java
+++ app/src/main/java/com/android/shashi/barcodescanner/Result.java
@@ -15,7 +15,7 @@ public class Result extends AppCompatActivity {
         String result = Objects.requireNonNull(Objects.requireNonNull(getIntent().getExtras()).get("result")).toString();
         String format = Objects.requireNonNull(Objects.requireNonNull(getIntent().getExtras()).get("format")).toString();
         TextView textView = findViewById(R.id.result);
-        String output = "Value: "+result+"\nFormat: "+format+"\nProduct Id: "+result.substring(0,4)+"\nRevision Id: "+result.substring(4,8)+"\nSerial Number: "+result.substring(8);
+        String output = "Value: "+result+"\nFormat: "+format;
         textView.setText(output);
 
     }
